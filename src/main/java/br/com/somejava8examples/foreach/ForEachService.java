77aeabec935056a4e256913d9443a8caffd7680e
Daniel Oliveira <danielso2007@gmail.com>
2019-12-16 16:55:15 -0300

    feat: Java 8 usando ForEach.

--- src/main/java/br/com/somejava8examples/foreach/ForEachService.java
+++ src/main/java/br/com/somejava8examples/foreach/ForEachService.java
@@ -1,5 +1,90 @@
 package br.com.somejava8examples.foreach;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class ForEachService {
+
+	public Map<String, Integer> normalWayToLoopMap() {
+		System.out.println("\nNormal way to loop a Map:");
+		Map<String, Integer> items = getItemsMap();
+
+		for (Map.Entry<String, Integer> entry : items.entrySet()) {
+			System.out.println("Item : " + entry.getKey() + " Count : " + entry.getValue());
+		}
+
+		return items;
+	}
+
+	public Map<String, Integer> normalWayToLoopMapLambdaExpression() {
+		System.out.println("\nNormal way to loop a Map with Lambda Expression:");
+		Map<String, Integer> items = getItemsMap();
+
+		items.forEach((k, v) -> System.out.println("Item : " + k + " Count : " + v));
+
+		items.forEach((k, v) -> {
+			System.out.println("Item : " + k + " Count : " + v);
+			if ("E".equals(k)) {
+				System.out.println(String.format("Hello %s - %d", k, v));
+			}
+		});
+
+		return items;
+	}
+
+	public List<String> forEachAndList() {
+		System.out.println("\nNormal for-loop to loop a List:");
+		List<String> items = getItemsList();
+
+		for (String item : items) {
+			System.out.println(item);
+		}
+
+		items = getItemsList();
+
+		System.out.println("\nLambda - Output : A,B,C,D,E:");
+		items.forEach(item -> System.out.println(item));
+
+		System.out.println("\nOutput: C");
+		items.forEach(item -> {
+			if ("C".equals(item)) {
+				System.out.println(item);
+			}
+		});
+
+		System.out.println("\nMethod reference - Output : A,B,C,D,E:");
+		items.forEach(System.out::println);
+
+		System.out.println("\nStream and filter - Output : B:");
+		items.stream().filter(s -> s.contains("B")).forEach(System.out::println);
+
+		return items;
+	}
+
+	private Map<String, Integer> getItemsMap() {
+		Map<String, Integer> items = new HashMap<>();
+		items.put("A", 10);
+		items.put("B", 20);
+		items.put("C", 30);
+		items.put("D", 40);
+		items.put("E", 50);
+		items.put("F", 60);
+		return items;
+	}
+
+	private List<String> getItemsList() {
+		List<String> items = new ArrayList<>();
+		items.add("A");
+		items.add("B");
+		items.add("C");
+		items.add("D");
+		items.add("E");
+		return items;
+	}
 
 }
---
+++ src/test/java/br/com/somejava8examples/comparator/ForEachTests.java
@@ -0,0 +1,33 @@
+package br.com.somejava8examples.comparator;
+
+import static org.springframework.util.Assert.isTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import br.com.somejava8examples.foreach.ForEachService;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ForEachTests {
+
+	@Autowired
+	private ForEachService service;
+
+	@Test
+	public void normalWayToLoopMapTest() {
+		isTrue(!service.normalWayToLoopMap().isEmpty(), "List is empty");
+	}
+
+	public void normalWayToLoopMapLambdaExpressionTest() {
+		isTrue(!service.normalWayToLoopMapLambdaExpression().isEmpty(), "List is empty");
+	}
+
+	@Test
+	public void forEachAndListTest() {
+		isTrue(!service.forEachAndList().isEmpty(), "List is empty");
+	}
+
+}
