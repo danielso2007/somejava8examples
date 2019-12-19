da525730bff26616a9de5aa88ccf911910ddb716
Daniel Oliveira <danielso2007@gmail.com>
2019-12-19 19:07:27 -0300

    doc: Facilitando a criação de versão do projeto.

---
+++ README.md
@@ -0,0 +1,15 @@
+[![Actions Status](https://github.com/danielso2007/somejava8examples/workflows/JavaCI/badge.svg)](https://github.com/danielso2007/somejava8examples/actions)
+
+![Coveralls github](https://img.shields.io/coveralls/github/danielso2007/somejava8examples)
+
+![GitHub repo size](https://img.shields.io/github/repo-size/danielso2007/somejava8examples)
+
+![GitHub contributors](https://img.shields.io/github/contributors/danielso2007/somejava8examples)
+
+![GitHub last commit (branch)](https://img.shields.io/github/last-commit/danielso2007/somejava8examples/master)
+
+![GitHub issues](https://img.shields.io/github/issues/danielso2007/somejava8examples)
+
+![GitHub pull requests](https://img.shields.io/github/issues-pr/danielso2007/somejava8examples)
+
+![GitHub All Releases](https://img.shields.io/github/downloads/danielso2007/somejava8examples/total)
---
+++ new_release.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+RED='\033[1;31m'
+BLACK='\033[0;30m'
+DARK_GRAY='\033[1;30m'
+LIGHT_RED='\033[0;31m'
+GREEN='\033[0;32m'
+LIGHT_GREEN='\033[1;32m'
+BROWN_ORANGE='\033[0;33m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+LIGHT_BLUE='\033[1;34m'
+PURPLE='\033[0;35m'
+LIGHT_PURPLE='\033[1;35m'
+CYAN='\033[0;36m'
+LIGHT_CYAN='\033[1;36m'
+LIGHT_GRAY='\033[0;37m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+set -e
+BRANCH=$1
+VERSION=$2
+DRYRUN=$3
+if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+    echo -e "${RED}Usage:${NC} ${LIGHT_RED}./new_release.sh [BRANCH] [VERSION]${NC} or ${LIGHT_RED}./new_release.sh [BRANCH] [VERSION] [test - DRY-RUN]${NC}"
+else
+    echo -e "${YELLOW}Branch:${NC} ${LIGHT_PURPLE}${BRANCH}${NC}"
+    echo -e "${YELLOW}Version:${NC} ${LIGHT_PURPLE}${VERSION}${NC}"
+    if [[ "$3" ]]; then
+        echo -e "${YELLOW}Dry-run:${NC} ${LIGHT_PURPLE}YES${NC}"
+        npm run release -- --release-as ${VERSION} --dry-run
+    else
+        npm run release -- --release-as ${VERSION}
+        git push --follow-tags origin ${BRANCH}
+    fi
+fi
