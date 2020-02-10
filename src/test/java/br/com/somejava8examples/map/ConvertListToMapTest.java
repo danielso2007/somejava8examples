c8e5d90eb9676107b89c6c0402e5014473449801
Daniel Oliveira <danielso2007@gmail.com>
2020-02-10 01:59:18 -0300

    teste de CI

--- src/test/java/br/com/somejava8examples/map/ConvertListToMapTest.java
+++ src/test/java/br/com/somejava8examples/map/ConvertListToMapTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
